3295a4b3fd32c0f682eabe88b8a0b6980cec8f63
Dulal-Ahmed <dulalaahmedjalal@gmail.com>
2023-02-17 17:25:43 +0600

    start changing

---
+++ js/Script.js
@@ -0,0 +1,45 @@
+//  import important array
+// top text input
+let textInput = document.querySelector('#textInput');
+//   text show in top 
+let storyHeader = document.querySelector('#storyHeader');
+//  middle left div img select
+let leftImage = document.querySelector('#leftImage');
+//   middile right div img select
+let rightImage = document.querySelector('#rightImage');
+let storybody = document.querySelector('#storybody');
+let storyText = document.querySelector('#storyText');
+let loadtxtBtN = document.querySelector('#loadtxtBtN');
+let loadNxtBTN = document.querySelector('#loadNxtBTN');
+//  Deafult load ( first load)
+let firstLoad = ()=>{
+    textInput.textContent = sipur1[0].text; 
+    leftImage.src = sipur1[0].image;
+}
+//  end first load function 
+//  every section text load function  
+// let exchangeDta =(datas)=>{
+//   console.log(datas.exchangeRates[0].key);
+// };
+let datas;
+let a = 0;
+let loadText =(n)=>{
+      a = a + n * 1; 
+      console.log(datas.exchangeRates[a].key);  
+      storyHeader.textContent = datas.exchangeRates[a].key;
+      storybody.textContent = datas.exchangeRates[a].currentExchangeRate;
+      storyText.textContent = datas.exchangeRates[a].currentChange;
+
+}
+//  First load funciton call for load deafult data 
+firstLoad();
+
+// read local JSON file in javascript
+fetch("./story/ExchangeRate.json")
+  .then((response)=>response.json())
+  .then((data)=> datas = data)
+   
+// text load button 
+   loadtxtBtN.addEventListener('click',()=>{
+    loadText(1);
+   });
---
+++ story/ImageandText.js
@@ -0,0 +1,39 @@
+
+const sipur1 = [
+    {
+        id: 1,
+        image: "images/usd1.jpg",
+        text: "כחזרתי הביתה אחר הצוהריים הרגשתי",
+    },
+    {
+        id: 2,
+        image: "images/apple1.png",
+        text: "רעב",
+    },
+    {
+        id: 3,
+        image: "images/juice1.png",
+        text: "צמא",
+    },
+    {
+        id: 4,
+        image: "images/gbp1.png",
+        text: "111",
+    },
+    {
+        id: 5,
+        image: "images/usd1.png",
+        text: "112",
+    },
+    {
+        id: 6,
+        image: "images/apple1.png",
+        text: "121",
+    },
+    {
+        id: 7,
+        image: "images/juice1.png",
+        text: "122",
+    }
+
+];
